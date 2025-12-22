a6d7caebb53084f5b5cb0578fd02980b35fea038
hafeelfaleel <hafeelfaleel@gmail.com>
2025-12-22 13:06:51 +0530

    CS50 week 2 submission 2

---
+++ Readability.c
@@ -0,0 +1,49 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 0;
+    int sentences = 0;
+
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+    words++;
+
+    float L = ((float) letters / words) * 100;
+    float S = ((float) sentences / words) * 100;
+
+    int index = round(0.0588 * L - 0.296 * S - 15.8);
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+}
